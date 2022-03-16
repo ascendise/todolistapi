af3586f0797d5142f9dde006512d0ba49b29bb4e
ascendise <60201207+ascendise@users.noreply.github.com>
2022-03-16 21:18:21 +0100

    Created Spring Project

---
+++ src/test/kotlin/ch/ascendise/todolistapi/TodolistApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package ch.ascendise.todolistapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodolistApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
