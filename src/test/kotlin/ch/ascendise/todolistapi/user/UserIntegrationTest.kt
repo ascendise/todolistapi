b2ba1caf1f16a6faa36a1d6e5bfbdfc7bd6351dc
ascendise <rapperido@gmail.com>
2022-03-23 15:08:49 +0100

    Test redirection to Google OAuth2

---
+++ src/test/kotlin/ch/ascendise/todolistapi/user/UserIntegrationTest.kt
@@ -0,0 +1,30 @@
+package ch.ascendise.todolistapi.user
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserIntegrationTest() {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc;
+
+    @Test
+    fun `Get redirected to Google OAuth2`()
+    {
+        mockMvc.perform(get("/oauth2/authorization/google"))
+            .andExpect(status().is3xxRedirection);
+    }
+
+}
