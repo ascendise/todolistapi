1c1cad14af3fb9257b28366bce5b49c41eb87c27
ascendise <60201207+ascendise@users.noreply.github.com>
2022-03-27 01:49:45 +0100

    Endpoint for returning current user information

--- src/test/kotlin/ch/ascendise/todolistapi/user/UserIntegrationTest.kt
+++ src/test/kotlin/ch/ascendise/todolistapi/user/UserIntegrationTest.kt
@@ -1,11 +1,20 @@
 package ch.ascendise.todolistapi.user
 
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.access.prepost.PostAuthorize
+import org.springframework.security.core.authority.AuthorityUtils
+import org.springframework.security.oauth2.core.oidc.OidcIdToken
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
 import org.springframework.security.test.context.support.WithAnonymousUser
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oidcLogin
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 
@@ -21,6 +30,9 @@ class UserIntegrationTest() {
     @Autowired
     private lateinit var mockMvc: MockMvc
 
+    @MockkBean
+    private lateinit var userRepository: UserRepository
+
     @Test
     fun `Get redirected to Google OAuth2`()
     {
@@ -34,6 +46,30 @@ class UserIntegrationTest() {
     {
         mockMvc.perform(get("/"))
             .andExpect(status().is3xxRedirection)
+    }
+
+    @Test
+    @PostAuthorize("authorized")
+    @WithMockUser(username = "Max Muster")
+    fun `Show info of current user`() {
+        val expectedUser = User(1, "maxmuster@mail.com", "Max Muster")
+        every { userRepository.findByEmail(expectedUser.email) } returns listOf(expectedUser)
+        val oidcUser = DefaultOidcUser(
+            AuthorityUtils.createAuthorityList("SCOPE_message:read"),
+            OidcIdToken.withTokenValue("id-token")
+                .claim("sub", "12345")
+                .claim("email", expectedUser.email)
+                .claim("given_name", expectedUser.username)
+                .build()
+        )
+        val result = mockMvc.perform(
+            get("/user").with(oidcLogin().oidcUser(oidcUser))
+        )
+            .andExpect(status().is2xxSuccessful)
+            .andReturn().response.contentAsString
+        assertAll({result.contains(expectedUser.email)},
+            {result.contains(expectedUser.username)},
+            {result.contains(expectedUser.id.toString())})
     }
 
 }
