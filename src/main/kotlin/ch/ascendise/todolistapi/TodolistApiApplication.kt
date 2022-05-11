7a761fcb43496a4fb1204b40c5e6f0d69d117e92
ascendise <rapperido@gmail.com>
2022-05-11 14:31:04 +0200

    Create template properties file if no properties file is present

--- src/main/kotlin/ch/ascendise/todolistapi/TodolistApiApplication.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/TodolistApiApplication.kt
@@ -2,10 +2,22 @@ package ch.ascendise.todolistapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @SpringBootApplication
 class TodolistApiApplication
 
 fun main(args: Array<String>) {
+	createPropertiesFileIfNotExists()
 	runApplication<TodolistApiApplication>(*args)
+}
+
+fun createPropertiesFileIfNotExists() {
+	val newFile = Paths.get("./application.yml")
+	if(!Files.exists(newFile)) {
+		val propertiesFileStream = TodolistApiApplication::class.java.getResourceAsStream("/templates/application_template.yml")
+			?: throw java.lang.RuntimeException("Template for application properties could not be loaded!");
+		Files.write(newFile, propertiesFileStream.readAllBytes())
+	}
 }
