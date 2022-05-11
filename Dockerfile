e8a78736115f652d2f25a95ab1cae1650856fc14
Ascendise <rapperido@gmail.com>
2022-05-11 22:53:15 +0200

    Run app as non-root user

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ RUN ./mvnw -Dmaven.test.skip package
 FROM openjdk:17-alpine
 WORKDIR /usr/bin/todolistapi/
 COPY --from=build /app/target/todolistapi-1.0.0.jar ./
+RUN addgroup -S spring && adduser -S spring -G spring
+USER spring:spring
 ENTRYPOINT ["java", "-jar", "./todolistapi-1.0.0.jar"]
--- src/main/kotlin/ch/ascendise/todolistapi/TodolistApiApplication.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/TodolistApiApplication.kt
@@ -1,12 +1,16 @@
 package ch.ascendise.todolistapi
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 
 @SpringBootApplication
 class TodolistApiApplication
+
+private val logger = LoggerFactory.getLogger(TodolistApiApplication::class.java)
 
 fun main(args: Array<String>) {
 	createPropertiesFileIfNotExists()
@@ -17,7 +21,14 @@ fun createPropertiesFileIfNotExists() {
 	val newFile = Paths.get("./application.yml")
 	if(!Files.exists(newFile)) {
 		val propertiesFileStream = TodolistApiApplication::class.java.getResourceAsStream("/templates/application_template.yml")
-			?: throw java.lang.RuntimeException("Template for application properties could not be loaded!");
-		Files.write(newFile, propertiesFileStream.readAllBytes())
+			?: throw java.lang.RuntimeException("Template for application properties could not be loaded!")
+		try
+		{
+			Files.write(newFile, propertiesFileStream.readAllBytes())
+		}
+		catch(ex: IOException)
+		{
+			logger.warn("Failed to create template application.yml file.", ex)
+		}
 	}
 }
