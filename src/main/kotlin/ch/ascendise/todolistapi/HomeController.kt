ede41effa9152a0872325c7365712e9212822b93
ascendise <rapperido@gmail.com>
2022-05-18 16:50:18 +0200

    Return links to available resources in root

---
+++ src/main/kotlin/ch/ascendise/todolistapi/HomeController.kt
@@ -0,0 +1,27 @@
+package ch.ascendise.todolistapi
+
+import ch.ascendise.todolistapi.checklist.ChecklistController
+import ch.ascendise.todolistapi.checklisttask.ChecklistTaskController
+import ch.ascendise.todolistapi.task.TaskController
+import ch.ascendise.todolistapi.user.CurrentUser
+import ch.ascendise.todolistapi.user.User
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.server.mvc.linkTo
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HomeController {
+
+    @GetMapping("/")
+    fun getLinks(@CurrentUser user: User) = EntityModel.of(Response(),
+        linkTo<TaskController> { getTasks(user) }.withRel("tasks"),
+        linkTo<ChecklistController> { getChecklists(user) }.withRel("checklists"),
+        linkTo<ChecklistTaskController> { getRelations(user) }.withRel("relations")
+    )
+
+    class Response
+    {
+
+    }
+}
