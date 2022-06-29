440ba8d9abd790639b8b97f6efead7e9c54d4c0c
ascendise <rapperido@gmail.com>
2022-06-29 13:57:59 +0200

    Fix errors with CORS

--- src/main/kotlin/ch/ascendise/todolistapi/SecurityConfiguration.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/SecurityConfiguration.kt
@@ -1,11 +1,16 @@
 package ch.ascendise.todolistapi
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
 
 @Configuration
 class SecurityConfiguration : WebSecurityConfigurerAdapter() {
@@ -13,6 +18,7 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity)
     {
         http
+            .cors().and()
             .csrf{ it.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) }
             .authorizeRequests{
                 it.antMatchers("/", "/login").permitAll()
@@ -31,4 +37,17 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
                 it.logoutSuccessUrl("/login")
             }
     }
+
+    @Bean
+    fun corsFilter(): CorsFilter? {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOriginPatterns = listOf("*")
+        config.allowedHeaders = listOf("*")
+        config.allowedMethods = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+
 }
