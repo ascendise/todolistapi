cd4acb4d0a2729a5ac2a1e7a47a3ef0d2a13c0b6
ascendise <60201207+ascendise@users.noreply.github.com>
2022-06-29 15:18:48 +0200

    fix: Fix issues with CORS (#18)
    * Fix errors with CORS
    
    * Able to specify allowed origin patterns in app properties
    
    * Update README.md

---
+++ src/main/kotlin/ch/ascendise/todolistapi/ApplicationConfig.kt
@@ -0,0 +1,13 @@
+package ch.ascendise.todolistapi
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
+class ApplicationConfig(
+    var allowedOriginPatterns: String = "*"
+) {
+}
