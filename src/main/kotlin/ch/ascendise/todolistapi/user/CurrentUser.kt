0e65c231a4f1504e9112cf033a84cd78d4d401de
Ascendise <rapperido@gmail.com>
2022-07-13 15:21:53 +0200

    Don't show User in request body

--- src/main/kotlin/ch/ascendise/todolistapi/user/CurrentUser.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/user/CurrentUser.kt
@@ -1,8 +1,10 @@
 package ch.ascendise.todolistapi.user
 
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
 @AuthenticationPrincipal(expression = "@userService.getUser(#this)")
+@Parameter(hidden = true)
 annotation class CurrentUser
