a90502c33699abbf0610caddf1cf59269a4bbe36
Ascendise <rapperido@gmail.com>
2022-07-13 02:37:26 +0200

    CSRF Protection no longer needed

--- src/main/kotlin/ch/ascendise/todolistapi/SecurityConfiguration.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/SecurityConfiguration.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.authentication.HttpStatusEntryPoint
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
 import org.springframework.web.cors.CorsConfiguration
@@ -22,8 +23,9 @@ class SecurityConfiguration : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity)
     {
         http
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .cors().and()
-            .csrf{ it.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) }
+            .csrf().disable()
             .authorizeRequests{
                 it.anyRequest().authenticated()
             }
