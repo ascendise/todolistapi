0ff804821047c71e40e4c56a1e3db13d60272662
Ascendise <rapperido@gmail.com>
2022-09-04 17:17:47 +0200

    Fixed deletion of tasks and checklists

--- src/main/kotlin/ch/ascendise/todolistapi/checklist/ChecklistRepository.kt
+++ src/main/kotlin/ch/ascendise/todolistapi/checklist/ChecklistRepository.kt
@@ -2,9 +2,10 @@ package ch.ascendise.todolistapi.checklist
 
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
+import javax.transaction.Transactional
 
 interface ChecklistRepository : JpaRepository<Checklist, Long> {
     fun findAllByUserId(userId: Long) : List<Checklist>
     fun findByIdAndUserId(id: Long, userId: Long) : Optional<Checklist>
-    fun deleteByIdAndUserId(id: Long, userId: Long)
+    @Transactional fun deleteByIdAndUserId(id: Long, userId: Long)
 }
