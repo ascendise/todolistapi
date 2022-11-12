8545d090f5466d502d9659e4d0c1e43df365528b
Ascendise <rapperido@gmail.com>
2022-11-12 19:35:49 +0100

    tests for UserController

---
+++ src/test/kotlin/ch/ascendise/todolistapi/user/UserControllerTest.kt
@@ -0,0 +1,21 @@
+package ch.ascendise.todolistapi.user
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class UserControllerTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun getCurrentUser() {
+    }
+
+    @Test
+    fun deleteCurrentUser() {
+    }
+}
